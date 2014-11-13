e1475ce4862e4cb06b56bd6b472d34a62352287d
Aggelos Giantsios <aggelgian@gmail.com>
2014-11-14 01:25:03 +0200

    Add stub Master module

---
+++ Master.hs
@@ -0,0 +1,9 @@
+--
+-- orbit-int master (controlling orbit computation)
+--
+module Master (now) where
+
+-- current wall clock time (in milliseconds since start of RTS)
+-- FIXME get current wall clock time
+now :: Int
+now = 42
