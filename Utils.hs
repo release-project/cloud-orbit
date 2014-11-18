182f08409ac2b3e42feefafa74fb983903ade818
Yiannis Tsiouris <gtsiour@softlab.ntua.gr>
2014-11-18 13:50:29 +0200

    Add comments of future implementation of now

--- Utils.hs
+++ Utils.hs
@@ -1,6 +1,9 @@
 module Utils (now) where
 
+--import System.CPUTime (getCPUTime)
+
 -- current wall clock time (in milliseconds since start of RTS)
 -- FIXME: get current wall clock time
+--now :: IO Integer
 now :: Int
-now = 42
+now = 42 --getCPUTime
