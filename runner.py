b4b4f70965c757749e4aa28cd41ce9e0ee8c7c88
Aggelos Giantsios <aggelgian@gmail.com>
2014-12-06 11:17:26 +0200

    Slaves will use 2 cores and we'll have up to 8 slaves
    This setup is for greedy.softlab.ntua.gr

--- runner.py
+++ runner.py
@@ -7,7 +7,7 @@ import subprocess, time
 def run_dist(master, slaves, version, workers, iwp):
   # Setup the slave nodes
   for slave in slaves:
-    cmd = "./orbit dist slave %s %s" % (slave["host"], slave["port"])
+    cmd = "./orbit +RTS -N2 -RTS dist slave %s %s" % (slave["host"], slave["port"])
     process = subprocess.Popen(cmd, shell=True)
   time.sleep(2)
   # Run the master node
@@ -24,7 +24,7 @@ def run_dist(master, slaves, version, workers, iwp):
 
 reps = 2
 iwp = False
-max_cpu = 32
+max_cpu = 16
 versions = ["short", "intermediate", "long"]
 master = {"host": "127.0.0.1", "port": 5050}
 slaves = [ {"host": "127.0.0.1", "port": 5051}
@@ -35,8 +35,6 @@ slaves = [ {"host": "127.0.0.1", "port": 5051}
          , {"host": "127.0.0.1", "port": 5056}
          , {"host": "127.0.0.1", "port": 5057}
          , {"host": "127.0.0.1", "port": 5058}
-         , {"host": "127.0.0.1", "port": 5059}
-         , {"host": "127.0.0.1", "port": 5060}
          ]
 
 f = open('statistics.txt', 'w')
@@ -46,7 +44,7 @@ f = open('statistics.txt', 'w')
 for nSlaves in range(1, len(slaves)+1):
   for iwp in [False, True]:
     for vsn in versions:
-      for n in range(2, max_cpu+1):
+      for n in range(2, 2*max_cpu+1, 2):
         slvs = slaves[0:nSlaves]
         print ("Slaves: %s, Workers: %s, Version: %s, IWP: %s" % (nSlaves, n, vsn, iwp), file=f)
         ts = []
