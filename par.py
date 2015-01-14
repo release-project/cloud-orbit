b8deabc2a81316807798c408c22539a406422336
Aggelos Giantsios <aggelgian@gmail.com>
2015-01-14 20:08:21 +0200

    Write output also to file

--- par.py
+++ par.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import subprocess, time
+from __future__ import print_function
+import sys, subprocess, time
+
+f = open('par.log', 'a+')
+def print_all(s):
+  print (s, file=f)
+  f.flush()
+  print (s, file=sys.stdout)
 
 reps = 1
 versions = ["short", "intermediate", "long"]
@@ -10,27 +17,27 @@ cores = [1,2,4]
 workersPerCore = 1
 node = {"host": "127.0.0.1", "port": 5050}
 
-print "Parallel Orbit"
-print "----------------------------------------------------------------------"
-print "Versions: %s" % versions
-print "Parallel Image Computation: %s" % iwps
-print "Repetitions per Configuration: %s" % reps
-print "Using Cores: %s" % cores
-print "Workers Per Core: %s" % workersPerCore
-print "Node @ %s" % node
-print "======================================================================"
+print_all("Parallel Orbit")
+print_all("----------------------------------------------------------------------")
+print_all("Versions: %s" % versions)
+print_all("Parallel Image Computation: %s" % iwps)
+print_all("Repetitions per Configuration: %s" % reps)
+print_all("Using Cores: %s" % cores)
+print_all("Workers Per Core: %s" % workersPerCore)
+print_all("Node @ %s" % node)
+print_all("======================================================================")
 
 for iwp in iwps:
   for vsn in versions:
     for core in cores:
       for rep in range(reps):
         workers = workersPerCore * core
-        print "Version: %s, IWP: %s, Cores: %s, Workers: %s, Execution: %s" % (vsn, iwp, core, workers, rep)
+        print_all("Version: %s, IWP: %s, Cores: %s, Workers: %s, Execution: %s" % (vsn, iwp, core, workers, rep))
         t1 = time.time()
         cmd = "./orbit +RTS -N%s -RTS par %s %s %s %s %s > /dev/null" % (core, iwp, vsn, workers, node["host"], node["port"])
         p = subprocess.Popen(cmd, shell=True)
         p.wait()
         t2 = time.time()
-        print "  %s sec(s)" % (t2 - t1)
+        print_all("  %s sec(s)" % (t2 - t1))
 
 
--- seq.py
+++ seq.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import subprocess, time
+from __future__ import print_function
+import sys, subprocess, time
+
+f = open('seq.log', 'a+')
+def print_all(s):
+  print (s, file=f)
+  f.flush()
+  print (s, file=sys.stdout)
 
 reps = 1
 versions = ["short", "intermediate", "long"]
 node = {"host": "127.0.0.1", "port": 5050}
 
-print "Sequential Orbit"
-print "----------------------------------------------------------------------"
-print "Versions: %s" % versions
-print "Repetitions per Configuration: %s" % reps
-print "Node @ %s" % node
-print "======================================================================"
+print_all("Sequential Orbit")
+print_all("----------------------------------------------------------------------")
+print_all("Versions: %s" % versions)
+print_all("Repetitions per Configuration: %s" % reps)
+print_all("Node @ %s" % node)
+print_all("======================================================================")
 
 for vsn in versions:
   for rep in range(reps):
-    print "Version: %s, Execution: %s" % (vsn, rep)
+    print_all("Version: %s, Execution: %s" % (vsn, rep))
     t1 = time.time()
     cmd = "./orbit seq %s %s %s > /dev/null" % (vsn, node["host"], node["port"])
     p = subprocess.Popen(cmd, shell=True)
     p.wait()
     t2 = time.time()
-    print "  %s sec(s)" % (t2 - t1)
+    print_all("  %s sec(s)" % (t2 - t1))
